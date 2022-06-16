1467860fce6f679ba9479017d313c1054787b9d1
Imran Khan <impathan007@gmail.com>
2022-06-16 11:42:28 +0530

    test

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {isAuthenticated, getAccessToken} from './session'
 
 // export const baseUrl = "http://localhost:8080/";
-export const baseUrl = "https://api.zoomable.tech/";
+export const baseUrl = "http://api.zoomable.tech/";
 
 export const zoomUrl = "https://api.zoom.us/v2";
 
